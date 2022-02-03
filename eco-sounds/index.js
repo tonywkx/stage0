8fb8a5b2b4a97c305d63817b0e364c6328e3ee4f
tonywkxe <tonywk@ya.ru>
2022-02-03 18:18:36 +0400

    feat: add changeAudio and changePic function

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,32 +1,148 @@
 
+//changeImage
 
-/*const navSolovey = document.querySelector('.solovey-item');
+const allItems = document.querySelectorAll('.nav-item');
+const navSolovey = document.querySelector('.solovey-item');
+const image = document.querySelector('.main');
 const navDrozd = document.querySelector('.drozd-item');
-const navZarynka = document.querySelector('zarynka-item');
+const navZarynka = document.querySelector('.zarynka-item');
 const navJavoronok = document.querySelector('.javoronok-item');
 const navSlavka = document.querySelector('.slavka-item');
-const logo = document.querySelector('.logo')*/
-
+const logo = document.querySelector('.logo')
 
 navSolovey.addEventListener('click', () =>{
-    mainPic.classList.add('solovey-active');
+    image.style.backgroundImage = 'url("./assets/img/solovey.jpg")';
+    let isPlay = false;
+    const audio = new Audio();
+    const playPauseBtn = document.querySelector('.audio-button');
+    function playAudio() {
+        audio.src = './assets/audio/solovey.mp3'
+        audio.currentTime = 0;
+        if(!isPlay){
+            audio.play();
+            isPlay = true;
+        } else {audio.pause();
+            isPlay = false;
+        }
+    
+    navSolovey.classList.add('nav-item-active');
+}
+playPauseBtn.addEventListener('click', playAudio);
+    
 });
+
+
+
 navDrozd.addEventListener('click', () =>{
-    mainPic.classList.add('drozd-active');
+    image.style.backgroundImage = 'url("./assets/img/drozd.jpg")';
+    let isPlay = false;
+    const audio = new Audio();
+    const playPauseBtn = document.querySelector('.audio-button');
+    function playAudio() {
+        audio.src = './assets/audio/drozd.mp3'
+        audio.currentTime = 0;
+        if(!isPlay){
+            audio.play();
+            isPlay = true;
+        } else {audio.pause();
+            isPlay = false;
+        }
+}
+playPauseBtn.addEventListener('click', playAudio);
 });
 
-const navLinks = document.querySelectorAll('.nav-item');
-const mainPic = document.querySelector('.main');
-const navList = document.querySelector('.nav-list')
+navZarynka.addEventListener('click', () =>{
+    image.style.backgroundImage = 'url("./assets/img/zarynka.jpg")';
+    let isPlay = false;
+    const audio = new Audio();
+    const playPauseBtn = document.querySelector('.audio-button');
+    function playAudio() {
+        audio.src = './assets/audio/zarynka.mp3'
+        audio.currentTime = 0;
+        if(!isPlay){
+            audio.play();
+            isPlay = true;
+        } else {audio.pause();
+            isPlay = false;
+        }
+}
+playPauseBtn.addEventListener('click', playAudio);
+});
 
-const portfolioBtn = document.querySelector('.button-container');
-const portfolioBtns = document.querySelectorAll('.portfolio-button');
+navJavoronok.addEventListener('click', () =>{
+    image.style.backgroundImage = 'url("./assets/img/javoronok.jpg")';
+    let isPlay = false;
+    const audio = new Audio();
+    const playPauseBtn = document.querySelector('.audio-button');
+    function playAudio() {
+        audio.src = './assets/audio/javoronok.mp3'
+        audio.currentTime = 0;
+        if(!isPlay){
+            audio.play();
+            isPlay = true;
+        } else {audio.pause();
+            isPlay = false;
+        }
+}
+playPauseBtn.addEventListener('click', playAudio);
+});
 
-const changeImage = function changeImage(event){
-    if(event.target.classList.contains('nav-item')){
-        mainPic.classList((img, index) => img.src = `assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
-        portfolioBtns.forEach((button) => button.classList.remove('portfolio-button-active'));
-        event.target.classList.add('portfolio-button-active');
-    }    
-} 
-portfolioBtn.addEventListener('click',  changeImage);  
+navSlavka.addEventListener('click', () =>{
+    image.style.backgroundImage = 'url("./assets/img/slavka.jpg")';
+    let isPlay = false;
+    const audio = new Audio();
+    const playPauseBtn = document.querySelector('.audio-button');
+    function playAudio() {
+        audio.src = './assets/audio/slavka.mp3'
+        audio.currentTime = 0;
+        if(!isPlay){
+            audio.play();
+            isPlay = true;
+        } else {audio.pause();
+            isPlay = false;
+        }
+}
+playPauseBtn.addEventListener('click', playAudio);
+});
+
+logo.addEventListener('click', () =>{
+    image.style.backgroundImage = 'url("./assets/img/logo.jpg")';
+});
+
+
+
+//playAudio
+
+let isPlay = false;
+const audio = new Audio();
+const playPauseBtn = document.querySelector('.audio-button');
+
+function playAudio() {
+    audio.src = './assets/audio/logo.mp3'
+    audio.currentTime = 0;
+    if(!isPlay){
+        audio.play();
+        isPlay = true;
+    } else {audio.pause();
+        isPlay = false;
+    }
+}
+playPauseBtn.addEventListener('click', playAudio);
+
+
+
+//changeButtonImage
+
+
+const button = document.querySelector('.audio-button');
+
+function toggleBtn() {
+  button.classList.toggle('pause');
+}
+button.addEventListener('click', toggleBtn);
+
+
+
+
+
+
