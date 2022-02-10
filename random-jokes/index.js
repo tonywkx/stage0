d4a6df6002f9c267ea63101fb69d9e374b47c8c7
tonywkxe <tonywk@ya.ru>
2022-02-10 18:06:17 +0400

    feat: done with task

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,16 +1,13 @@
 //getQuotes
 
-const url = 'https://type.fit/api/quotes/';
+let url = 'https://type.fit/api/quotes/';
 const newText = document.querySelector('p');
-
-
 
 async function getQ() {
     const resQ = await fetch(url);
     const data = await resQ.json();
     console.log(data);    
     showQ(data);
-
 }
 getQ();
 
@@ -24,8 +21,7 @@ function showQ(data) {
 
 function randomInteger(min, max) {
     min = 0;
-    max = 1641;
-    
+    max = 99;
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
@@ -33,13 +29,9 @@ function randomInteger(min, max) {
 //changeQuotes
 
 const button = document.querySelector('.btn');
-
 button.addEventListener('click', getQ);
 
-
-
 //changeImg&BG
-
 
 function randomColor() {
     let color = Math.floor(Math.random() * 256 * 256 * 256);
@@ -53,6 +45,26 @@ function changeBG() {
     document.body.style.backgroundColor = str;
 }
 
-
 //language
 
+const en = document.querySelector('.en');
+const ru = document.querySelector('.ru');
+
+async function getRuQuotes() {  
+    const quotes = 'quotes.json';
+    const res = await fetch(quotes);
+    const dataRu = await res.json(); 
+    console.log(dataRu);
+    
+  }
+getRuQuotes();
+
+function changeRuSrc(){
+    url = 'quotes.json';
+}
+ru.addEventListener('click', changeRuSrc);
+
+function changeEnSrc() {
+    url = 'https://type.fit/api/quotes/';
+}
+en.addEventListener('click', changeEnSrc);
